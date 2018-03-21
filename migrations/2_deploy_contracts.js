1b360d1456d73b2444aca585f65624c1bfc2ac2b
Andy M <admazzola@gmail.com>
2018-03-21 08:48:20 -0400

    test can deploy now

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
-var _0xBitcoinToken = artifacts.require("./_0xBitcoinToken.sol");
+var ECRecovery = artifacts.require("./ECRecovery.sol");
+
+var LavaWallet = artifacts.require("./LavaWallet.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(_0xBitcoinToken);
+  deployer.deploy(ECRecovery);
+
+
+    deployer.link(ECRecovery, LavaWallet)
+
+  deployer.deploy(LavaWallet);
+
 };
