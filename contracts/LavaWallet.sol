39c97d55b879a56cecdbdda2c7c0d46e2e9593c5
Andy M <admazzola@gmail.com>
2018-04-05 16:34:22 -0400

    addign auto wrapping and unwrapping , compiles

--- contracts/LavaWallet.sol
+++ contracts/LavaWallet.sol
@@ -16,65 +16,147 @@ contract ERC20Interface {
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
+
+//wEth interface
+contract WrapperInterface
+{
+  function() public payable;
+  function deposit() public payable ;
+  function withdraw(uint wad) public;
+  function totalSupply() public view returns (uint);
+  function approve(address guy, uint wad) public returns (bool);
+  function transfer(address dst, uint wad) public returns (bool);
+  function transferFrom(address src, address dst, uint wad);
+
+
+  event  Approval(address indexed src, address indexed guy, uint wad);
+  event  Transfer(address indexed src, address indexed dst, uint wad);
+  event  Deposit(address indexed dst, uint wad);
+  event  Withdrawal(address indexed src, uint wad);
+
+}
+
+
+
+
+
 contract LavaWallet {
 
 
-      using SafeMath for uint;
+  using SafeMath for uint;
 
   // balances[tokenContractAddress][EthereumAccountAddress] = 0
   mapping(address => mapping (address => uint256)) balances;
-  mapping(bytes32 => uint256) spentSignatures;
+  // mapping(address => mapping (address => uint256)) allowed;
+   mapping(bytes32 => uint256) spentSignatures;
 
+
+  event Deposit(address token, address user, uint amount, uint balance);
+  event Withdraw(address token, address user, uint amount, uint balance);
+  event Transfer(address indexed from, address indexed to,address token, uint tokens);
+  event Approval(address indexed tokenOwner, address indexed spender,address token, uint tokens);
 
   function LavaWallet() public  {
 
   }
 
 
-  function balanceOf(address tokenContract,address tokenOwner) public constant returns (uint balance) {
-       return balances[tokenContract][tokenOwner];
-   }
-
-   function signatureSpent(bytes32 digest) public view returns (bool)
-   {
-     return (spentSignatures[digest] == 0x0);
-   }
+  //do not allow ether to enter
+  function() public payable {
+      revert();
+  }
 
 
-   function invalidateSignature(address to, uint256 tokens, address token, uint256 checkNumber, bytes32 sigHash, bytes signature) public returns (bool)
-   {
-       address recoveredSignatureSigner = ECRecovery.recover(sigHash,signature);
-
-       //maker sure the invalidator is the signer
-       if(recoveredSignatureSigner != msg.sender) revert();
-
-       bytes32 sigDigest = keccak256(to, tokens, token, checkNumber);
-
-       if(sigDigest != sigHash) revert();
-
-       //make sure this signature has never been used
-       uint spentSignature = spentSignatures[sigDigest];
-       spentSignatures[sigDigest] = 0x2; //invalidated
-       if(spentSignature != 0x0 ) revert();
-
-       return true;
-   }
-
-
-   //performed via an ApproveAndCall
-  function _depositTokens(address from, uint256 tokens, address token) internal returns (bool)
+  //send Ether into this method, it gets wrapped and then deposited in this contract as a token balance assigned to the sender
+  function depositAndWrap(address wrappingContract) public payable
   {
-      if(msg.sender != token) revert(); //must come from ApproveAndCall
-      if(tokens <= 0) revert(); //need to deposit some tokens
+    //convert the eth into wEth
+
+    //send this payable ether into the wrapping contract
+    wrappingContract.transfer(msg.value);
+
+    //send the tokens from the wrapping contract to here
+    WrapperInterface(wrappingContract).transfer(this, msg.value);
+
+    balances[wrappingContract][msg.sender] = balances[wrappingContract][msg.sender].add(msg.value);
+
+    assert(this.balance == 0); //make sure it is not a faulty wrapping contract
+
+    Deposit(wrappingContract, msg.sender, msg.value, balances[wrappingContract][msg.sender]);
+  }
+
+  //when this contract has control of wrapped eth, this is a way to easily withdraw it as ether
+  function unwrapAndWithdraw(address token, uint256 tokens) public
+  {
+      //transfer the tokens into the wrapping contract which is also the token contract
+      transfer(token,token,tokens);
+
+      WrapperInterface(token).withdraw(tokens);
+
+      //send ether to the token-sender
+      msg.sender.transfer(tokens);
+
+      assert(this.balance == 0); //make sure it is not a faulty wrapping contract
+
+      Withdraw(token, msg.sender, tokens, balances[token][msg.sender]);
+
+  }
+
+
+   //remember you need pre-approval for this - nice with ApproveAndCall
+  function depositToken(address from, address token, uint256 tokens) public returns (bool)
+  {
+    ///  if(msg.sender != token) revert(); //must come from ApproveAndCall
+      if(token <= 0) revert(); //need to deposit some tokens
 
       //we already have approval so lets do a transferFrom - transfer the tokens into this contract
       ERC20Interface(token).transferFrom(from, this, tokens);
       balances[token][from] = balances[token][from].add(tokens);
 
+      Deposit(token, from, tokens, balances[token][from]);
+
       return true;
   }
 
-  function withdrawTokensFrom(address from, uint256 tokens, address token, uint256 checkNumber, bytes32 sigHash, bytes signature) public returns (bool)
+  function withdrawToken(address token, uint256 tokens) {
+    if(token <= 0) revert();
+    if (balances[token][msg.sender] < tokens) revert();
+
+    balances[token][msg.sender] = balances[token][msg.sender].sub(tokens);
+
+    ERC20Interface(token).transfer(msg.sender, tokens);
+
+    Withdraw(token, msg.sender, tokens, balances[token][msg.sender]);
+  }
+
+  function balanceOf(address token,address user) public constant returns (uint) {
+       return balances[token][user];
+   }
+
+ function transfer(address to, address token, uint tokens) public returns (bool success) {
+      balances[token][msg.sender] = balances[token][msg.sender].sub(tokens);
+      balances[token][to] = balances[token][to].add(tokens);
+      Transfer(msg.sender, token, to, tokens);
+      return true;
+  }
+
+/*  function approve(address spender, address token, uint tokens) public returns (bool success) {
+       allowed[token][msg.sender][spender] = tokens;
+       Approval(msg.sender, token, spender, tokens);
+       return true;
+   }
+
+
+   function transferFrom( address from, address to,address token,  uint tokens) public returns (bool success) {
+       balances[token][from] = balances[token][from].sub(tokens);
+       allowed[token][from][msg.sender] = allowed[token][from][msg.sender].sub(tokens);
+       balances[token][to] = balances[token][to].add(tokens);
+       Transfer(token, from, to, tokens);
+       return true;
+   }*/
+
+   //allows transfer without approval as long as you get an EC signature
+  function transferFromWithSignature(address from, uint256 tokens, address token, uint256 checkNumber, bytes32 sigHash, bytes signature) public returns (bool)
   {
       //check to make sure that signature == ecrecover signature
 
@@ -105,6 +187,34 @@ contract LavaWallet {
       return true;
   }
 
+
+
+     function signatureSpent(bytes32 digest) public view returns (bool)
+     {
+       return (spentSignatures[digest] != 0x0);
+     }
+
+
+     function invalidateSignature(address to, uint256 tokens, address token, uint256 checkNumber, bytes32 sigHash, bytes signature) public returns (bool)
+     {
+         address recoveredSignatureSigner = ECRecovery.recover(sigHash,signature);
+
+         //maker sure the invalidator is the signer
+         if(recoveredSignatureSigner != msg.sender) revert();
+
+         bytes32 sigDigest = keccak256(to, tokens, token, checkNumber);
+
+         if(sigDigest != sigHash) revert();
+
+         //make sure this signature has never been used
+         uint spentSignature = spentSignatures[sigDigest];
+         spentSignatures[sigDigest] = 0x2; //invalidated
+         if(spentSignature != 0x0 ) revert();
+
+         return true;
+     }
+
+
    /*
      Receive approval to spend tokens and perform any action all in one transaction
    */
@@ -115,7 +225,7 @@ contract LavaWallet {
 
    if(action_id == 0x1)
    {
-     return _depositTokens(from, tokens, token);
+     return depositToken(from, token, tokens);
    }
 
    return false;
