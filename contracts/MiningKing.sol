7e9818eaae284e9693510491dabfe6e796f11791
Andy M <admazzola@gmail.com>
2018-07-20 15:47:56 -0400

    comment

--- contracts/MiningKing.sol
+++ contracts/MiningKing.sol
@@ -1,7 +1,52 @@
 pragma solidity ^0.4.18;
 
 
-import "./SafeMath.sol";
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  /**
+  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
 
 /*
@@ -139,7 +184,7 @@ Set the king to the Ethereum Address which is encoded as 160 bits of the 256 bit
       return true;
    }
 
-   
+
   function popFirstFromArray(address[] array) pure public returns (address[] memory)
   {
     address[] memory newArray = new address[](array.length-1);
